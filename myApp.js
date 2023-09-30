c0ddf15a9f2a6b196a74638dfadcfbb6540e6137
haydaraliawan <85960376+haydaraliawan@users.noreply.github.com>
2023-10-01 00:17:06 +0500

    css files worked using middleware

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 let express = require('express');
 let app = express();
+const path = require('path');
 // let absolutePath = path.join(__dirname, '/views/index.html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname+'/views/index.html');
 })
