804e073fa01dc533c1e160f907c17fb19e08c557
haydaraliawan <85960376+haydaraliawan@users.noreply.github.com>
2023-10-02 09:37:43 +0500

    jdjd

--- myApp.js
+++ myApp.js
@@ -25,7 +25,7 @@ app.get('/now', function(req, res, next) {
     req.time = new Date().toString();
     next();
   }, function(req, res) {
-    res.json(req.time);
+    res.json({ time: req.time })
   });
 
 // Route handler for /now
