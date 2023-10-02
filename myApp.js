104bad8b3ce640f4ef4c7cb28d04853a0d51154a
haydaraliawan <85960376+haydaraliawan@users.noreply.github.com>
2023-10-02 08:19:48 +0500

    root level middleware active

--- myApp.js
+++ myApp.js
@@ -3,12 +3,12 @@ let express = require("express");
 let app = express();
 const path = require("path");
 // let absolutePath = path.join(__dirname, '/views/index.html');
-app.use("/public", express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
     const logString = `${req.method} ${req.path} - ${req.ip}`;
     console.log(logString);
     next();
 });
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/views/index.html");
 });
