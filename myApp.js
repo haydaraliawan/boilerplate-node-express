33c7a66c4d538f7368e712bb471b8543bb25f60c
haydaraliawan <85960376+haydaraliawan@users.noreply.github.com>
2023-10-02 08:06:44 +0500

    built a logger

--- myApp.js
+++ myApp.js
@@ -1,59 +1,26 @@
-require('dotenv').config();
-let express = require('express');
+require("dotenv").config();
+let express = require("express");
 let app = express();
-const path = require('path');
+const path = require("path");
 // let absolutePath = path.join(__dirname, '/views/index.html');
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use((req, res, next) => {
+    const logString = `${req.method} ${req.path} - ${req.ip}`;
+    console.log(logString);
+    next();
+});
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+});
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname+'/views/index.html');
-})
-
-
-app.get('/json', function (req, res) {
-    let resObj = {"message": "Hello json"};
+app.get("/json", function (req, res) {
+    let resObj = { message: "Hello json" };
     console.log(process.env.MESSAGE_STYLE);
-    if(process.env.MESSAGE_STYLE == "uppercase"){
-        res.json({"message": "HELLO JSON"});
-    }else{
-        
-        res.json({"message": "Hello json"});
+    if (process.env.MESSAGE_STYLE == "uppercase") {
+        res.json({ message: "HELLO JSON" });
+    } else {
+        res.json({ message: "Hello json" });
     }
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
