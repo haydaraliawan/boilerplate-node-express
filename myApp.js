b1ab7fda86f09e2a87a29d666271dda29df84ec3
haydaraliawan <85960376+haydaraliawan@users.noreply.github.com>
2023-10-02 09:05:43 +0500

    lll

--- myApp.js
+++ myApp.js
@@ -4,15 +4,15 @@ const app = express();
 const path = require("path");
 
 // Root level middleware to log requests
+
+// Serve static files from the "public" directory
+app.use("/public", express.static(path.join(__dirname, "public")));
+
 app.use((req, res, next) => {
   const logString = `${req.method} ${req.path} - ${req.ip}`;
   console.log(logString);
   next();
 });
-
-// Serve static files from the "public" directory
-app.use("/public", express.static(path.join(__dirname, "public")));
-
 // Define routes
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "views", "index.html"));
