99fa54044ca13471e7227c0a10bb2a48368c95b0
haydaraliawan <85960376+haydaraliawan@users.noreply.github.com>
2023-10-02 09:35:56 +0500

    chaining middleware

--- myApp.js
+++ myApp.js
@@ -9,32 +9,39 @@ const path = require("path");
 app.use("/public", express.static(path.join(__dirname, "public")));
 
 // app.use((req, res, next) => {
-    
+
 //     console.log(req.method + " " + req.path + " - " + req.ip);
 //   next();
 // });
 
-app.use('/now', (req, res, next) => {
+// app.get("/now", (req, res, next) => {
+//     req.time = new Date().toString();
+//     next(), function (req, res) {
+//         res.json({ time: req.time });
+//     };
+// });
+
+app.get('/now', function(req, res, next) {
     req.time = new Date().toString();
-    next(); // Call next to pass the modified request object to the next middleware/handler
+    next();
+  }, function(req, res) {
+    res.json(req.time);
   });
-  
-  // Route handler for /now
-  app.get('/now', (req, res) => {
-    res.json({ time: req.time }); // Respond with a JSON object containing the time
-  });
+
+// Route handler for /now
+
 // Define routes
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+    res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 app.get("/json", function (req, res) {
-  let resObj = { message: "Hello json" };
-  if (process.env.MESSAGE_STYLE === "uppercase") {
-    res.json({ message: "HELLO JSON" });
-  } else {
-    res.json({ message: "Hello json" });
-  }
+    let resObj = { message: "Hello json" };
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        res.json({ message: "HELLO JSON" });
+    } else {
+        res.json({ message: "Hello json" });
+    }
 });
 
 module.exports = app;
